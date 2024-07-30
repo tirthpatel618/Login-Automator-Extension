5ad5e44a1c28dfd3550650b01d39b4f02b9d4f84
Tirth Patel <t83patel@uwaterloo.ca>
2024-07-30 12:43:27 -0400

    Initial Commit

---
+++ Login Automator/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  Login Automator
+//
+//  Created by Tirth Patel on 2024-07-30.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    func applicationDidFinishLaunching(_ notification: Notification) {
+        // Override point for customization after application launch.
+    }
+
+    func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
+        return true
+    }
+
+}
---
+++ Login AutomatorUITests/Login_AutomatorUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Login_AutomatorUITestsLaunchTests.swift
+//  Login AutomatorUITests
+//
+//  Created by Tirth Patel on 2024-07-30.
+//
+
+import XCTest
+
+final class Login_AutomatorUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
