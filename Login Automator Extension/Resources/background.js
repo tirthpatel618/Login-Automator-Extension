b09752d9ea7fbdbed70bd8f8c94e8fc8c7e5a598
Tirth Patel <t83patel@uwaterloo.ca>
2024-08-01 09:58:35 -0400

    First commit - code extractor complete

--- Login Automator Extension/Resources/background.js
+++ Login Automator Extension/Resources/background.js
@@ -1,6 +1,73 @@
-browser.runtime.onMessage.addListener((request, sender, sendResponse) => {
-    console.log("Received request: ", request);
+import Imap;
+import { simpleParser };
+import util;
 
-    if (request.greeting === "hello")
-        sendResponse({ farewell: "goodbye" });
+var imap = new Imap({
+  user: 'tirth618@gmail.com',
+  password: 'ugxd rfzq sxvj jfqa',
+  host: 'imap.gmail.com',
+  port: 993,
+  tls: true,
+  tlsOptions: {
+    rejectUnauthorized: false
+  }
 });
+
+function openInbox(cb) {
+  imap.openBox('INBOX', true, cb);
+}
+imap.once('ready', function() {
+    openInbox(function(err, box) {
+        if (err) throw err;
+        const emailID = box.messages.total;
+        console.log(emailID);
+        const email = imap.seq.fetch(11805, { bodies: '' });
+        console.log('Fetching email');
+        email.on('message', (msg, seqno) => {
+            msg.on('body', (stream) => {
+                simpleParser(stream, async (err, mail) => {
+                    if (err) throw err;
+                    // Extract and log the email subject
+                    const emailSubject = mail.text;
+                    console.log('type', typeof emailSubject);
+                    console.log('Email Subject:', emailSubject);
+                    const codeMatch = emailSubject.match(/\b\d{6}\b/);
+                    if (codeMatch) {
+                      const verificationCode = codeMatch[0];
+                      console.log('Verification Code:', verificationCode);
+                      browser.runtime.sendMessage({code: verificationCode});
+
+                    } else {
+                      console.log('Verification Code not found');
+                    }
+                    // Your code to extract and process the email content here
+                    // Extract and log the email body
+                    //const emailBody = mail.html;
+                    //console.log('Email Body:', emailBody);
+                });
+            });
+        });
+        email.once('error', function(err) {
+            console.log('Fetch error: ' + err);
+        });
+        email.once('end', function() {
+            console.log('Done fetching all messages!');
+            imap.end();
+        });
+    });
+             
+});
+
+imap.once('error', function(err) {
+    console.log(err);
+});
+  
+imap.once('end', function() {
+    console.log('Connection ended');
+});
+  
+imap.connect();
+
+
+
+
--- Login Automator Extension/Resources/popup.js
+++ Login Automator Extension/Resources/popup.js
@@ -1,1 +1,8 @@
-console.log("Hello World!", browser);
+console.log("Code: ", browser);
+console.log('popupppp');
+browser.runtime.onMessage.addListener((request, sender, sendResponse) => {
+    console.log("Received request: ", request);
+    console.log(request.code);
+    document.getElementById('popup_message').innerHTML = request.code;
+});
+
