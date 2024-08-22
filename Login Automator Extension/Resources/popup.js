13e2ffd1c609d20319e11973b7fe9eb493713db9
Tirth Patel <t83patel@uwaterloo.ca>
2024-08-22 10:24:38 -0400

    popup issue identified

--- Login Automator Extension/Resources/popup.js
+++ Login Automator Extension/Resources/popup.js
@@ -4,7 +4,8 @@ browser.runtime.onMessage.addListener((request, sender, sendResponse) => {
 
 function replace_code(){
     console.log("button clicked")
-    document.getElementById('popup_text').innerHTML = "CODE";
+    document.getElementById('popup_text').innerHTML = "GENERATED CODE";
+    //js function to replace text
     console.log("replaced text")
 }
 
